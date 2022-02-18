b7033d8de3c91a834199e675f3ace957bd3647b8
Sarawuth Pimsai <82835119+sarawuth-pimsai@users.noreply.github.com>
2022-02-18 15:39:39 +0700

    Check between branch

--- .github/actions/semver/index.js
+++ .github/actions/semver/index.js
@@ -2999,6 +2999,8 @@ const cmd = async (commandLine, ...args) => {
 async function run() {
   try {
     let branch = core.getInput("branch", { required: true }); //output hash git commit
+    let branchDevelop = await cmd(`git rev-parse develop`);
+    let branchMain = await cmd(`git rev-parse main`);
     if (branch === "HEAD") {
       branch = (await cmd("git", "rev-parse", "HEAD")).trim();
     }
@@ -3008,7 +3010,7 @@ async function run() {
       await cmd(`git tag --points-at ${branch} *[0-9].*[0-9].*[0-9]`)
     ).trim();
     console.log("current tag", currentTag);
-    let logCommand = `git log --pretty="%s" --author-date-order ${branch}`;
+    let logCommand = `git log --pretty="%s" --author-date-order ${branchDevelop}..${branch}`;
     const logs = await cmd(logCommand);
     console.log("logs", logs);
     const logChangedFiles = await cmd(
