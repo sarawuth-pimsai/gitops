a8ccda44df6c096df7eb716e61bd2aaacf02e461
Sarawuth Pimsai <82835119+sarawuth-pimsai@users.noreply.github.com>
2022-02-18 11:39:30 +0700

    Add action payload

---
+++ payload.js
@@ -0,0 +1,11 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  const trigger = core.getInput("trigger");
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  core.setOutput("payload", payload);
+  core.setOutput("trigger", trigger);
+} catch (error) {
+  core.setFailed(error.message);
+}
